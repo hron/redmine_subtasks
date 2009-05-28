4afd1eef29e9bdfc03ecbe6baab9104f5a40e5f3
Aleksei Gusev <aleksei.gusev@gmail.com>
2009-05-28 14:18:40 +0300

    removed debugger execution from the source.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -90,7 +90,6 @@ class IssuesController < ApplicationController
 
       def retrieve_query_with_subtasks
         retrieve_query_without_subtasks
-        debugger
         if params[:query_id].blank?
           if params[:set_filter]
             if params[:view_options] and params[:view_options].is_a? Hash
